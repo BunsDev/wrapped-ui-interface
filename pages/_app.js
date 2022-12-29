6f6125f93297dc28da21d876445fb7cbbb4f660b
johnswaroop <71548636+johnswaroop@users.noreply.github.com>
2022-12-29 13:43:21 +0530

    mobile screens complete

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
 import '../styles/globals.css'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Script type="module" src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js">
+    </Script>
+    <Component {...pageProps} /></>
 }
